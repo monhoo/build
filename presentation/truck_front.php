a2e72ae73461fe63633edb7b795d654495be3d42
monhoo <tsgtbal@yahoo.com>
2015-11-02 15:51:52 -0600

    updated php file

--- presentation/truck_front.php
+++ presentation/truck_front.php
@@ -2,20 +2,15 @@
 class TruckFront
 {
 public $mSiteUrl;
-<<<<<<< HEAD
 public $mMyWord;
-=======
->>>>>>> b5fff0647d25873b998811318293bdd7fb0b85fb
-
 // Class constructor
 public function __construct(){
 $this->mSiteUrl = Link::Build('');
-<<<<<<< HEAD
+
 $this->mMyWord = "this is freaking good";
 //print_r($this->mSiteUrl);
-=======
 print_r($this->mSiteUrl);
->>>>>>> b5fff0647d25873b998811318293bdd7fb0b85fb
+
 }
 // Initialize presentation object
 public function init(){}
