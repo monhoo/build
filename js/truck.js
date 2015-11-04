264f139acf34c6c1e69feec46f8bebedef7ebb87
monhoo <tsgtbal@yahoo.com>
2015-11-03 20:09:27 -0600

    dfgfgdfg

--- js/truck.js
+++ js/truck.js
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-  $(window).on("load", function() {
+  $(body).on("load", function() {
        
 $(".container").load("front_page.php?" + $.param({
         AjaxRequest: true,
