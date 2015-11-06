8a0a5b4c3f67f5db227802e0bb6bbc25ccc3a3ce
monhoo <tsgtbal@yahoo.com>
2015-11-05 21:07:54 -0600

    fghgfh

--- front_page.php
+++ front_page.php
@@ -12,25 +12,7 @@ require_once ("include/config.php");
 // Load the application page template
 require_once ("presentation/application.php");
 require_once ("pesentation/link.php");
-// Load the database handler
-//require_once BUSINESS_DIR . 'database_handler.php';
-// Load Business Tier
-/*require_once BUSINESS_DIR . 'catalog.php';
-require_once BUSINESS_DIR . 'shopping_cart.php';
-require_once BUSINESS_DIR . 'password_hasher.php';
-require_once BUSINESS_DIR . 'symmetric_crypt.php';
-require_once BUSINESS_DIR . 'secure_card.php';
-require_once BUSINESS_DIR . 'customer.php';
-require_once BUSINESS_DIR . 'orders.php';
-require_once BUSINESS_DIR . 'order_processor.php';
-require_once BUSINESS_DIR . 'i_pipeline_section.php';
-require_once BUSINESS_DIR . 'ps_initial_notification.php';
-require_once BUSINESS_DIR . 'ps_check_funds.php';
-require_once BUSINESS_DIR . 'ps_take_payment.php';//added later
-require_once BUSINESS_DIR . 'ps_check_stock.php';
-require_once BUSINESS_DIR . 'ps_ship_goods.php';//added later
-require_once BUSINESS_DIR . 'authorize_net_request.php';
-require_once BUSINESS_DIR . 'amazon.php';*/
+
 
 // URL correction
 Link::CheckRequest();
@@ -45,7 +27,7 @@ if (isset ($_GET["CartAction"]))
 {
 echo(include("presentation/truck_front.php"));
 $truck = new TruckFront();
-
+$application->display('presentation/templates/truck_front.tpl');
 echo'<div style="width:20px;height:30px;" class="bg-danger">iopopoipiop</div>';
 echo(file_get_contents("presentation/templates/truck_front.tpl"));
 }
