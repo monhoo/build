36e39ae366c7be2e217cca6cf5ef2a6b7565c1da
monhoo <tsgtbal@yahoo.com>
2015-11-05 21:22:18 -0600

    fghfg

--- include/config.php
+++ include/config.php
@@ -2,13 +2,13 @@
 // SITE_ROOT contains the full path to the tshirtshop folder
 define('SITE_ROOT', dirname(dirname(__FILE__)));
 // Application directories
-define('PRESENTATION_DIR', "/presentation/");
-define('BUSINESS_DIR', "/business/");
+define('PRESENTATION_DIR', SITE_ROOT . '/presentation/');
+define('BUSINESS_DIR', SITE_ROOT . '/business/');
 // Settings needed to configure the Smarty template engine
-define('SMARTY_DIR', "/libs/smarty/");
-define('TEMPLATE_DIR', "templates");
-define('COMPILE_DIR', "templates_c");
-define('CONFIG_DIR', "../include/configs");
+define('SMARTY_DIR', SITE_ROOT . '/libs/smarty/');
+define('TEMPLATE_DIR', PRESENTATION_DIR . 'templates');
+define('COMPILE_DIR', PRESENTATION_DIR . 'templates_c');
+define('CONFIG_DIR', SITE_ROOT . '/include/configs');
 //define('SSL_CERTIFICATION', SITE_ROOT . '/include/api.sandbox.paypal.com.pem');
 //print_r( SSL_CERTIFICATION);
 // These should be true while developing the web site
